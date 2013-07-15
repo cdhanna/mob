fecfa797d41c3a5891bd71d4b51f1519f45d2278
Chris Hanna <cdhanna@wpi.edu>
2013-07-14 23:19:43 -0400

    added base package and test class

---
+++ Mobsters/src/com/hanna/mobsters/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.hanna.mobsters;
+
+/**
+ * @author Chris Hanna
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Make him an offer he can't refuse.");
+	}
+
+}
